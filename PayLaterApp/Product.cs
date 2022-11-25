3931ff46a59ebacee5c49d9f3317ab50fd591285
Ebube4im Anya <ebube4im@gmail.com>
2022-11-25 08:59:51 +0100

    Add project files.

---
+++ PayLaterApp/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayLaterApp
+{
+    internal class Product
+    {
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+
+        public int Price { get; set; }
+
+
+
+        public static List<Product> GetProductList()
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product(){ ProductName = "Mac Book Pro 2022" , ProductDescription = "Latest edition of Apple Mac Book", Price= 1500000},
+                new Product(){ ProductName = "Dell Xps 2021" , ProductDescription = "Latest edition of Dell", Price= 2000000},
+                new Product(){ ProductName = "Lenovo Lappad" , ProductDescription = "Latest edition of Lenovo", Price= 1000000},
+                new Product(){ ProductName = "Asus Air" , ProductDescription = "Latest edition of Asus Laptop", Price= 1200000},
+                new Product(){ ProductName = "Hp Slim X" , ProductDescription = "Latest edition of Hp Laptops", Price= 1300000},
+            };
+
+            return products;
+        }
+    }
+}
---
+++ PayLaterApp/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace PayLaterApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Jumia Back Side Store");
+            Console.WriteLine("Which product will you love to buy!");
+
+         Console.WriteLine("Product Name                            |   Product Description       |    Price  ");
+
+            foreach (var product in Product.GetProductList())
+            {
+                Console.WriteLine($"1. {product.ProductName}  |  {product.ProductDescription} |  {product.Price}");
+            }
+        }
+    }
+}
